5ee4618d6d3037475678a6ad3005b5e298c8b37b
Aariton94 <123118477+Aariton94@users.noreply.github.com>
2025-10-17 13:44:58 +0100

    push application to git

--- src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.example.ExpenseTracker;
+package com.example.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
